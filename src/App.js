aa04d06c793efc79e0e2c5f7cd1a034a75d27375
Kenneth Ehidiamhen <scottspark@Kenneths-MacBook-Pro.local>
2025-06-25 21:18:09 +0100

    Add Tawk.to live chat widget to all pages

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { useEffect } from 'react';
+
+function App() {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://embed.tawk.to/685c582e71a033190a6217df/1iukbgtn6';
+    script.async = true;
+    script.charset = 'UTF-8';
+    script.setAttribute('crossorigin', '*');
+    document.body.appendChild(script);
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  return (
+    <div>
+      {/* Your app content goes here */}
+    </div>
+  );
+}
+
+export default App; 
